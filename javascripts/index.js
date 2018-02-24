4bfa5c64e1d3f646641508f387b4e0822daeff89
Yoohogyun <yoohoogun114@naver.com>
2018-02-24 11:24:22 +0900

    stash

--- javascripts/index.js
+++ javascripts/index.js
@@ -11,6 +11,8 @@ let audio = new Audio('./../heartbeat1seconds.mp3');
 
 let beforeHeartBeat = 0;
 
+let triggerFlag = false;
+
 socket.on('connect', () => {
   
 })
@@ -18,10 +20,20 @@ socket.on('connect', () => {
 socket.on('heartbeat', (_heartbeat) => { // response got every 1 seconds
   let thisHeartbeat = parseInt(_heartbeat);
   console.log('now heartbeat is', thisHeartbeat);
+  // console.log('triggerFlag is', triggerFlag);
   if(beforeHeartBeat === 0 && thisHeartbeat !== 0 ) {
     animation.play();
   }
   if(thisHeartbeat > 0) { // valid
+    let totalFrame = Math.round(animation.totalFrames, 2);
+    let currentFrame = Math.round(animation.currentRawFrame, 2);
+    // console.log('totalFrame is', totalFrame);
+    // console.log('currentFrame is', currentFrame);
+
+    if(totalFrame - currentFrame == 0 || totalFrame - currentFrame == totalFrame) { // 정지되어 있는 경우
+      // play
+      
+    }
     animation.stop();
     animation.setSpeed(thisHeartbeat/60);    
     animation.play();
@@ -31,3 +43,7 @@ socket.on('heartbeat', (_heartbeat) => { // response got every 1 seconds
     beforeHeartBeat = 0;
   }
 })
+
+// audio.load();
+// audio.play();
+// 1초마다 트리거, 속도는 조정
